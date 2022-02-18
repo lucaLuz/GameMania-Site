b7ecfa4ffbe39483ace96a3640fda38f06eddcb3
LucaLuz <luiza24021@gmal.com>
2022-02-18 19:28:50 -0300

    teste502

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
-function mostrarPopup(){
-    window.alert("Ola Viviane")
-}
+// function mostrarPopup(){
+//     window.alert("Ola Mundo")
+// }
+// $(document) .ready(function(){
+//     $("#barras").click(function(){
+//         if($(#menu).hasClass("#menu-ativo")){
+//             $("#menu").removeClass("#menu-ativo")
+//         }else{
+//             $("#menu").addClass("#menu-ativo")
+//         }
+//     }
+//     )
+// }
+// )
+
+
+
+
+$( document ).ready(function() {
+    $("#barras").click(function () {
+        $("#menu").toggleClass("menu-ativo")
+    })
+})
